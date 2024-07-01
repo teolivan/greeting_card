2d3473e0b95a6ecf0ae3c701ea64c2cf9a41daf2
olivia svensson <oliviateoliva@gmail.com>
2024-07-01 15:08:11 +0200

    add

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Olivia",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
