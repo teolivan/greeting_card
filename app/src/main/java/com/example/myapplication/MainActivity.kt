bafdb4ae4b4cf9cea531694f946818df427fe2b5
olivia svensson <oliviateoliva@gmail.com>
2024-07-02 11:57:23 +0200

    add

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +15,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,20 +38,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message : String, from : String, to : String, modifier : Modifier = Modifier) {
-Column(modifier = modifier){
+Column(modifier = modifier.padding(8.dp),
+    verticalArrangement = Arrangement.Center){
     Text (
         text = "From " + from,
-        fontSize = 26.sp,
+        fontSize = 36.sp,
+        modifier = Modifier
+            .padding(16.dp)
         )
     Text (
         text = message,
         fontSize = 60.sp,
-        lineHeight = 80.sp
+        lineHeight = 80.sp,
+        textAlign = TextAlign.Center
     )
     Text (
         text = "To " + to,
         fontSize = 30.sp,
-        lineHeight = 50.sp
+        lineHeight = 50.sp,
+        textAlign = TextAlign.Center
+
     )
 }
 
@@ -62,6 +71,10 @@ fun BirthdayCardPreview() {
     }
 }
 
+@Composable
+fun BirthdayImage(message : String, from : String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+}
 
 
 
